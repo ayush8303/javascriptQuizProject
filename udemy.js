ff486d0b52231281547abac66491be23c1fd9065
Ayush Tripathi <arnavtripathi1234@gmail.com>
2023-02-23 10:38:45 +0530

    added code

--- udemy.js
+++
@@ -1,6 +0,0 @@
-const form = document.querySelector(".feedback-form");
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  console.log(form.username.value);
-  console.log(form.feedback.value);
-});
